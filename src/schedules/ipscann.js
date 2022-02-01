a29cc89c2ba99627cb2268c5ad29413bb5dd5369
victor <victor@MacBook-Pro-de-victor-2.local>
2022-02-01 20:18:39 -0300

    nuetas estructuras en llamados a BD

--- src/schedules/ipscann.js
+++ src/schedules/ipscann.js
@@ -3,8 +3,16 @@ const express = require('express');
 const router = express.Router();
 const utils = require('../utils/utils.js');
 
-const task = cron.schedule(`*/30 * * * *`, () => {
-    console.log('running a task every 30 minutes');
+const mysqlConnection = require('../database.js');
+
+const SCHEDULED_TIME_STACK = process.env.SCHEDULED_TIME_STACK;
+
+router.post('/scheduler', async (req, res) => {
+    task.start();
+});
+
+const task = cron.schedule(`*/${SCHEDULED_TIME_STACK} * * * *`, () => {
+    console.log(`running a task every ${SCHEDULED_TIME_STACK} minutes`);
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     utils.getNewPublicIp().then(res => {
@@ -13,8 +21,5 @@ const task = cron.schedule(`*/30 * * * *`, () => {
     console.log(today.toISOString());
 });
 
-router.post('/scheduler', async (req, res) => {
-    task.start();
-});
 
 module.exports = router;
