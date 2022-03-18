3bca4ca2bdc9942f9283eb1f99682c1d572f4cfe
victor <victor@MacBook-Pro-de-victor-2.local>
2022-03-18 13:45:14 -0300

    cambios

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -8,6 +8,21 @@ const mysqlConnection = require('../database.js');
 const auth = require("../middleware/auth");
 
 routerWebHook.get('/webhook', (req, resp) => {
+    // const { number } = req.body;
+
+    // const width = 20
+    // var length = number.toString().length; /* Largo del número */
+    // var zero = "0"; /* String de cero */
+
+    // console.log(width > length);
+    // if (width > length) {
+    //     let response = ((zero.repeat(width - length)) + number.toString());
+    //     console.log(response);
+    //     resp.send(response);
+    // }else {
+    //     resp.send(number);
+    // }
+
     utils.sendTextSlackNotification('PRUEBA').then((data) => {
         resp.send((data))
     });
