40ede77d04b2b71c9155fd9c8b401b3a0c6db324
victor <victor@MacBook-Pro-de-victor-2.local>
2022-06-28 14:49:05 -0400

    se agrega variable de entorno en auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,22 +1,23 @@
 // Import dependencies
-const jwt = require("jsonwebtoken");
-const IS_PRODUCTION = process.env.IS_PRODUCTION;
+const jwt = require("jsonwebtoken")
+const IS_PRODUCTION = process.env.IS_PRODUCTION
+const JWT_KEY = process.env.JWT_KEY
 
 module.exports = (req, res, next) => {
-    const token = req.header("x-auth-token");
+    const token = req.header("x-auth-token")
     if(JSON.parse(IS_PRODUCTION)){
         if (!token) return res.status(401).send({
             error: "unauthorized"
         });
     
         try {
-            const decoded = jwt.verify(token, "jwtPrivateKey");
-            req.user = decoded;
+            const decoded = jwt.verify(token, JWT_KEY);
+            req.user = decoded
         } catch (error) {
             return res.status(401).send({
                 ok: false,
                 error: "Token expired"
-            });
+            })
         }
     }
 
