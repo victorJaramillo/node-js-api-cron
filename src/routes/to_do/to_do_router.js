ea25da2b8ec368ebc732561564528be90acdd114
victor <victor@MacBook-Pro-de-victor-2.local>
2023-05-11 00:39:32 -0400

    new endpoint

--- src/routes/to_do/to_do_router.js
+++ src/routes/to_do/to_do_router.js
@@ -18,10 +18,10 @@ router.get('/tasks', [auth_apikey], async (req, res) => {
 })
 router.post('/tasks', [auth_apikey], async (req, res) => {
     const { task_name } = req.body
-    if(task_name == undefined){
-        message = {error:400, message: "body params is required"}
+    if (task_name == undefined) {
+        message = { error: 400, message: "body params is required" }
         res.status(400).send(message)
-    }else {
+    } else {
         const bodyTosave = { task_name: `${task_name}`, task_id: `${utils.UUID()}` }
         try {
             var response = await query(queryUtils.save_chores_to_do, bodyTosave)
@@ -36,15 +36,14 @@ router.post('/tasks', [auth_apikey], async (req, res) => {
 router.put('/tasks/:id', [auth_apikey], async (req, res) => {
     const { id } = req.params
     const { status } = req.body
-    if(status == undefined){
-        message = {error:400, message: "body params is required"}
+    if (status == undefined) {
+        message = { error: 400, message: "body params is required" }
         res.status(400).send(message)
-    }else {
+    } else {
         try {
-            const bodyRequest = { status: `${status}` }
-            const { affectedRows } = await query(queryUtils.update_chores_to_do(id), bodyRequest)
-            if(affectedRows ===1){
-                res.send({message: `task updated successfully`})
+            const { affectedRows } = await query(queryUtils.update_chores_to_do(id), { status: status })
+            if (affectedRows === 1) {
+                res.send({ message: `task updated successfully` })
             }
         } catch (error) {
             res.status(400).send(JSON.parse(error))
